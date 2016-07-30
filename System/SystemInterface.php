d9cdc870a2d01392a75af9f11d00545a0ddde9ae
Khelle <jamroz.kamil@gmail.com>
2016-07-30 19:45:40 +0200

    KRF-233 #resolve Refactored System API && moved to Util\System

---
+++ System/SystemInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Kraken\Util\System;
+
+interface SystemInterface
+{
+    /**
+     * Run command asynchronously, and get pid of its process.
+     *
+     * @param string $command
+     * @return string
+     */
+    public function run($command);
+
+    /**
+     * Kill process with given pid, returns whether operation was successful.
+     *
+     * @param string $pid
+     * @return bool
+     */
+    public function kill($pid);
+
+    /**
+     * Checks if process with given pid exists, returns whether operation was successful.
+     *
+     * @param string $pid
+     * @return bool
+     */
+    public function existsPid($pid);
+}
